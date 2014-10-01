5fdbe15b51172344206e1c4f940fb29dd39a4c83
zcapamu <zcapamu@CT002102.ad.ucl.ac.uk>
2014-10-01 12:01:55 +0100

    Built-in Data Types and Operators and Functions/Methods sections of Module 1 complete.

--- src/module1/DataTypes.java
+++ src/module1/DataTypes.java
@@ -3,19 +3,39 @@ package module1;
 public class DataTypes {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+		
 		double doubVar = 10.0;
-		System.out.println(doubVar);
-		
-		float floatVar = 10.0f;
-		System.out.println(floatVar);
-		
-		System.out.println(doubVar*floatVar);
-
-		char charVar = 'a' + 10;
-		System.out.println(charVar);
+		System.out.println("Double: " + doubVar);
 		System.out.println('\n');
 		
+		//Convert double to float
+		float floatVar = 10.0f;
+		System.out.println("Double 10.0 to float: " + floatVar);
+		System.out.println('\n');
+		
+		//Multiply double with float
+		System.out.println("Double 10.0 x float 10.0: " + doubVar*floatVar);
+		System.out.println('\n');
+		
+		//Convert double to byte
+		byte byteVar = (byte) 10.0;
+		System.out.println("Byte 10.0: " + byteVar);
+		System.out.println("Byte 10.0 muliplied by itself: " + byteVar*byteVar);
+		System.out.println('\n');
+		
+		//Convert double to int
+		int intVar = (int) 10.0;
+		System.out.println("int 10.0: "+intVar);
+		System.out.println("int 10.0 multiplied by itself: "+intVar*intVar);
+		System.out.println('\n');
+		
+		//Convert double to long
+		long longVar = (long) 10.0;
+		System.out.println("long 10.0: "+longVar);
+		System.out.println("long 10.0 multiplied by itself: "+longVar*longVar);
+		System.out.println('\n');
+		
+		//Mix data types int and char assigned to int
 		int intChar = 7*'a';
 		int intA = (int) 'a';
 		System.out.println("int value of 'a': "+intA);
@@ -24,6 +44,7 @@ public class DataTypes {
 		System.out.println("Mixing types char and int in an int converts the char to an int and then performs the operation specified, multiplication in this case.");
 		System.out.println('\n');
 		
+		//Mix int and char assigned to char
 		char charInt = 117+'a';
 		char char7 = (char) 117;
 		System.out.println("char value of 117: "+char7);
@@ -36,6 +57,7 @@ public class DataTypes {
 		System.out.println("Mixing types char and int in a char converts the char to an int and then performs the operation specified, addition in this case, then converts the result back into a char.");
 		System.out.println('\n');
 		
+		//Mix double and char assigned to double
 		double pi = 3.1415;
 		double doubChar = pi +'a';
 		double doubA = (double) 'a';
@@ -46,7 +68,17 @@ public class DataTypes {
 		System.out.println("Mixing types double and char in a double converts the char to a double (which is its int value as a double) and then performs the operation specified, addition in this case.");
 		System.out.println('\n');
 		
-		int j = 1;
+		//Test result of using uninitialised variables
+		int j=1;
+		//The following lines will result  in a compile-time error as java cannot compile the code with ininitialised variables;
+		//int i;
+		//int j=i+1;
+		
+		//Converting double to int
+		double doubVar2 = 4.99;
+		int intDoub = (int) doubVar2;
+		System.out.println("Double 4.99 converted to int = " + intDoub);
+		
 		
 	}
 
