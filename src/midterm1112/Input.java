76101ed8449a716434591b81398fb20e64a6d649
adstastic <adstastic@icloud.com>
2014-11-10 12:53:13 +0000

    Finished exam1112 (except last part, don't understand how to do)

---
+++ src/midterm1112/Input.java
@@ -0,0 +1,55 @@
+package midterm1112;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.InputMismatchException;
+import java.util.List;
+import java.util.Map;
+import java.util.Scanner;
+import java.util.TreeMap;
+
+public class Input {
+	
+	static Scanner scFromURL(String url) throws IOException {
+		URL u = new URL(url);
+		Scanner sc = new Scanner(new BufferedReader(new InputStreamReader(u.openStream())));
+		return sc;
+	}
+	
+	public static Map<Double, BPM> getData(String url) throws IOException, InputMismatchException {
+		Map<Double, BPM> beam = new TreeMap<Double, BPM>();
+		Scanner sc = scFromURL(url);
+		String line;
+		String[] lineArray;
+		while (sc.hasNextLine()) {
+			BPM detector = new BPM();
+			line = sc.nextLine().trim();
+			if (Character.isLetter(line.charAt(0))) {}	
+			else {
+				lineArray = line.trim().split("\\s+");
+				if (lineArray.length != 3) { 
+					throw new InputMismatchException("Input must contain 3 variables.");
+				} else {
+					Double x = Double.parseDouble(lineArray[0]);
+					Double y = Double.parseDouble(lineArray[1]);
+					Double z = Math.abs(Double.parseDouble(lineArray[2]));
+					Particle p = new Particle(x, y, z);
+					//System.out.println(p);
+					if (beam.containsKey(z) == false) {
+						detector.add(p);
+						beam.put(z, detector);
+					} else {
+						beam.get(z).add(p);
+					}
+				}
+			}
+		}
+		sc.close();
+		return beam;
+	}
+
+}
+
